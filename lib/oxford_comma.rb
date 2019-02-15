6c460d76a8f6050da4bc019fb42c54b9a760d678
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-15 20:14:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,12 @@
 def oxford_comma(array)
-  result = array.join(", ")
-  result[result.rindex(",")] = " and"
+  if array.size == 1
+    result = array[0]
+  elsif array.size == 2
+    result = array.join(" and ")
+  else 
+    result = array.join(", ")
+    result[result.rindex(",")] = " and"
+    result
+  end
   result
 end
